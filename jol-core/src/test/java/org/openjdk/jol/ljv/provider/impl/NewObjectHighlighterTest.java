1b720596fa82e403026c76f245c92fbe45b24f15
Victor Petrosyan <viko20000@mail.ru>
2022-03-08 22:50:02 +0300

    sub module ljv in jol-core

---
+++ jol-core/src/test/java/org/openjdk/jol/ljv/provider/impl/NewObjectHighlighterTest.java
@@ -0,0 +1,18 @@
+package org.openjdk.jol.ljv.provider.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.openjdk.jol.ljv.provider.impl.NewObjectHighlighter.HIGHLIGHT;
+
+class NewObjectHighlighterTest {
+    @Test
+    void newObjectsAreHighlighted() {
+        Object o1 = new Object();
+        Object o2 = new Object();
+        NewObjectHighlighter highlighter = new NewObjectHighlighter();
+        Assertions.assertEquals(HIGHLIGHT, highlighter.getAttribute(o1));
+        Assertions.assertEquals(HIGHLIGHT, highlighter.getAttribute(o2));
+        Assertions.assertEquals("", highlighter.getAttribute(o1));
+    }
+}
