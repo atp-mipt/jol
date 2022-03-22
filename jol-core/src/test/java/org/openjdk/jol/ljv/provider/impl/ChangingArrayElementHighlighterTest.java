601d09cea08989a0039ab313a965a463b1aa53d1
Victor Petrosyan <viko20000@mail.ru>
2022-03-22 09:27:01 +0300

    problems with java 1.7(changed to 1.8) #9

--- jol-core/src/test/java/org/openjdk/jol/ljv/provider/impl/ChangingArrayElementHighlighterTest.java
+++ jol-core/src/test/java/org/openjdk/jol/ljv/provider/impl/ChangingArrayElementHighlighterTest.java
@@ -1,16 +1,19 @@
 package org.openjdk.jol.ljv.provider.impl;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assumptions;
+import org.openjdk.jol.ljv.VersionGuardedTest;
 
 import static org.openjdk.jol.ljv.provider.impl.ChangingArrayElementHighlighter.HIGHLIGHT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ChangingArrayElementHighlighterTest {
+class ChangingArrayElementHighlighterTest implements VersionGuardedTest {
 
     ChangingArrayElementHighlighter provider = new ChangingArrayElementHighlighter();
 
     @Test
     void checksChangedElements() {
+        Assumptions.assumeTrue(is11());
         int[] arr = new int[]{1, 2, 3};
         for (int i = 0; i < arr.length; i++) {
             assertEquals("", provider.getAttribute(arr, i));
--- jol-core/src/test/java/org/openjdk/jol/ljv/provider/impl/NewObjectHighlighterTest.java
+++ jol-core/src/test/java/org/openjdk/jol/ljv/provider/impl/NewObjectHighlighterTest.java
@@ -1,13 +1,16 @@
 package org.openjdk.jol.ljv.provider.impl;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.openjdk.jol.ljv.VersionGuardedTest;
 
 import static org.openjdk.jol.ljv.provider.impl.NewObjectHighlighter.HIGHLIGHT;
 
-public class NewObjectHighlighterTest {
+public class NewObjectHighlighterTest implements VersionGuardedTest {
     @Test
     void newObjectsAreHighlighted() {
+        Assumptions.assumeTrue(is11());
         Object o1 = new Object();
         Object o2 = new Object();
         NewObjectHighlighter highlighter = new NewObjectHighlighter();
